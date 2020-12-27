81fa9b5be1ddce4c793d44133b50c84ad5fbfff2
Naveen Rawat <naveen.rawat@sprint.com>
2020-12-27 11:12:05 +0530

    account created using phone number with cloud functions

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,10 @@
+const admin = require('firebase-admin');
+const serviceAccount = require('./serviceAccountKey.json');
 const functions = require('firebase-functions');
+const createUser = require('./create_user');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    functions.logger.info('Hello logs!', { structuredData: true });
-    response.send('Hello from Firebase!');
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
 });
+
+exports.createUser = functions.https.onRequest(createUser);
